4c3ab8806f83ca15560eea297f789e5a1794714c
Simo Aleksandrov <simo3003@me.com>
2018-07-10 17:41:39 +0300

    Add binary search

---
+++ main.js
@@ -0,0 +1,20 @@
+function binarySearch(arr, num) {
+    let startIndex = 0; // first element
+    let endIndex = arr.length - 1; // last element
+
+    while (startIndex < endIndex) {
+        let guessedIndex = Math.floor((startIndex + endIndex) / 2); // the middle num between our start and end index is our guess
+        let guessedNumber = arr[guessedIndex];
+
+        if (guessedNumber == num) { // if we have found the num
+            return guessedIndex; // return the index
+        } else {
+            if (guessedNumber > num) { // if the guessed number is too big
+                endIndex = guessedIndex - 1; // start again until the index BEFORE the guessed num
+            } else { // else the guessed number was too small
+                startIndex = guessedIndex + 1; // start again at the index AFTER the guessed num
+            }
+        }
+    }
+    return undefined;
+}

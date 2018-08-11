deb15797c5f7ce36c1c3ba7b7d15c2955053dc6b
Simo Aleksandrov <simo3003@me.com>
2018-08-11 14:30:17 +0300

    Finish Flood-Fill, crashes with larger images

--- Flood-Fill/main.py
+++ Flood-Fill/main.py
@@ -2,12 +2,15 @@ from PIL import Image
 import numpy as np
 import sys
 
+# increase recursion depth
+sys.setrecursionlimit(500000)
+
 # get args
 file_path = sys.argv[1]
 
 # arrays go by rows first THEN columns
-row = int(sys.argv[3]) 
-col = int(sys.argv[2]) 
+row = int(sys.argv[3])
+col = int(sys.argv[2])
 
 # get img
 print('Loading image...')
@@ -32,9 +35,9 @@ print(f'{file_path} successfully loaded!')
 
 def flood_fill(arr, row, col):
     # if the coords are out of bounds
-    if row >= height or col >= width or row < 0  or col < 0:
+    if row >= height or col >= width or row < 0 or col < 0:
         return
-    
+
     # if there is no pixel on these coords (False value)
     if arr[row][col] == False:
         # fill it in (invert)
@@ -42,17 +45,19 @@ def flood_fill(arr, row, col):
     else:
         return
 
-    # if there is no pixel on the coords surrounding this pixel, call recursively   
+    # if there is no pixel on the coords surrounding this pixel, call recursively
     if arr[row-1][col] == False:
-         flood_fill(arr, row-1, col)
+        flood_fill(arr, row-1, col)
     if arr[row][col+1] == False:
         flood_fill(arr, row, col+1)
-    if arr[row+1][row] == False:
-        flood_fill(arr, row+1, row)
+    if arr[row+1][col] == False:
+        flood_fill(arr, row+1, col)
     if arr[row][col-1] == False:
         flood_fill(arr, row, col-1)
 
+
 # call the method
+print('Starting flood fill...')
 flood_fill(arr, row, col)
 
 # invert values (True values become False and vice versa)
@@ -61,3 +66,6 @@ arr = np.logical_not(arr)
 # converts to RGB image and saves
 result = Image.fromarray(arr.reshape(width, height).astype('uint8')*255)
 result.save('result.png')
+
+# print final message
+print(f'{file_path} successfully flood filled at {col}, {row}! Check result.png!')

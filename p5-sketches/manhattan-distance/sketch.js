3b9d844753f9dc38b0836c6bcd098c7c33a7a43a
Simo Aleksandrov <simo3003@me.com>
2019-06-06 00:58:59 +0300

    we getting somewhere...

--- p5-sketches/manhattan-distance/sketch.js
+++ p5-sketches/manhattan-distance/sketch.js
@@ -1,8 +1,8 @@
 const canvasWidth = 800;
-const squareSize = 100;
 const squareThickness = 25;
 const canvasHeight = canvasWidth;
-const squareAmount = canvasHeight / squareSize;
+const squareAmount = 6;
+const squareSize = Math.ceil(canvasHeight / squareAmount);
 const squares = [];
 
 let start;
@@ -13,6 +13,7 @@ let manhattanDistance;
 let distP;
 
 function setup() {
+  console.log(squareSize);
   const c = createCanvas(
     canvasWidth + 3 * squareThickness,
     canvasHeight + 3 * squareThickness,
@@ -41,6 +42,33 @@ function draw() {
     stroke(0, 255, 0);
     strokeWeight(10);
     line(start.x, start.y, end.x, end.y);
+
+    // draw manhattan distance
+
+    // start is to the left of end
+    let maxLines = manhattanDistance / squareSize; 
+    if (start.x <= end.x) {
+      // start is under end
+      if (start.y <= end.y) {
+        let prevEnd = createVector(start.x, start.y);
+        stroke(0,0,255);
+        for (let i = 0; i <= maxLines; i++) {
+          if (i % 2 == 0) {
+            line(prevEnd.x, prevEnd.y, prevEnd.x + squareSize, prevEnd.y)
+            prevEnd.x = prevEnd.x + squareSize;
+          } else {
+            line(prevEnd.x, prevEnd.y, prevEnd.x, prevEnd.y + squareSize)
+            prevEnd.y = prevEnd.y + squareSize;
+          }
+        }
+      } else {
+      }
+    } else {
+      // start is under end
+      if (start.y <= end.y) {
+      } else {
+      }
+    }
   }
 
   if (start) {
@@ -114,12 +142,12 @@ function handleClick() {
 
   if (start && end) {
     euclideanDistance = floor(dist(start.x, start.y, end.x, end.y));
-    manhattanDistance = abs(start.x - end.x) + abs(start.y - end.y);
+    manhattanDistance = floor(abs(start.x - end.x) + abs(start.y - end.y));
 
     distP.html(`
      Euclidean Distance: ${euclideanDistance}
      </br>
      Manhattan Distance: ${manhattanDistance}
-    `)
+    `);
   }
 }

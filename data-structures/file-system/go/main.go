2d857a09d180c9a3724d459ffd4766e9628a6cf6
Simo Aleksandrov <simo3003@me.com>
2019-06-23 00:53:07 +0300

    test creating and reading files

--- data-structures/file-system/go/main.go
+++ data-structures/file-system/go/main.go
@@ -38,4 +38,16 @@ func main() {
 
 	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
 
+	fs.CreateFile("kernel", []byte("hello"))
+
+	content, _ := fs.ReadFile("kernel")
+	fmt.Println(string(content))
+
+	fs.ChangeDir("usr")
+
+	fs.CreateFile("nested", []byte("some content"))
+	fs.ChangeDir("../")
+
+	content, _ = fs.ReadFile("usr/nested")
+	fmt.Println(string(content))
 }

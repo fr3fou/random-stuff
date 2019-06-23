3f7679a360711bc3837ab1b352e781e16218cb5d
Simo Aleksandrov <simo3003@me.com>
2019-06-23 16:03:28 +0300

    update main.go

--- data-structures/file-system/go/main.go
+++ data-structures/file-system/go/main.go
@@ -9,106 +9,93 @@ func main() {
 	fs := filesystem.New()
 
 	fs.CreateDir("usr")
-	fs.CreateDir("usr/share")
-	fs.CreateDir("usr/local")
-	fs.ChangeDir("usr/local")
+	fs.ChangeDir("usr")
 
-	fs.ChangeDir("../../../")
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
 
-	items, _ := fs.ListDirectoryContents("usr")
+	fs.CreateDir("share")
+	fs.ChangeDir("share")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.ChangeDir("../..")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.ChangeDir("usr/share/")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.ChangeDir("/usr/share")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.ChangeDir("..")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.ChangeDir("/")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.CreateFile("kernel", []byte("hello"))
+
+	content, _ := fs.ReadFile("kernel")
+	fmt.Println(string(content))
+
+	fs.ChangeDir("usr")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	fs.CreateFile("nested", []byte("some content"))
+	fs.ChangeDir("../")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	content, _ = fs.ReadFile("usr/nested")
+	fmt.Println(string(content))
+
+	fs.CreateFile("/usr/share/testing", []byte("testing root files"))
+
+	content, _ = fs.ReadFile("/usr/share/testing")
+	fmt.Println(string(content))
+
+	fs.CreateDir("/usr/share/fog")
+	fs.ChangeDir("/usr/share/fog")
+
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	// shouldn't work as the directories preceding it don't exist
+	fs.CreateDir("/usr/share/fog/this/shouldnt-work")
+	fs.ChangeDir("/usr/share/fog/this/shouldnt-work")
+
+	fs.DeleteFile("/usr/share/testing")
+	// shouldn't work as the file has been deleted
+	content, _ = fs.ReadFile("/usr/share/testing")
+
+	fs.ChangeDir("..")
+	fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+
+	items, _ := fs.ListDirectoryContents(fs.PrintWorkingDirectory())
 
 	for k, v := range items {
 		fmt.Printf("%v - %+v\n", k, v)
 	}
 
-	// fs.CreateDir("usr")
-	// fs.ChangeDir("usr")
+	fs.DeleteDirectory("/usr/share/fog")
+	// shouldn't work as the directory has been deleted
+	items, err := fs.ListDirectoryContents("/usr/share/fog")
+	fmt.Println(err)
 
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
+	fs.CreateFile("kernel2", []byte("hey!"))
+	fs.EditFile("kernel2", []byte("hey again!"))
+	content, _ = fs.ReadFile("kernel2")
+	fmt.Println(string(content))
 
-	// fs.CreateDir("share")
-	// fs.ChangeDir("share")
+	fmt.Println(fs.PrintWorkingDirectory())
+	items, _ = fs.ListDirectoryContents(fs.PrintWorkingDirectory())
 
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.ChangeDir("../..")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.ChangeDir("usr/share/")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.ChangeDir("/usr/share")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.ChangeDir("..")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.ChangeDir("/")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.CreateFile("kernel", []byte("hello"))
-
-	// content, _ := fs.ReadFile("kernel")
-	// fmt.Println(string(content))
-
-	// fs.ChangeDir("usr")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// fs.CreateFile("nested", []byte("some content"))
-	// fs.ChangeDir("../")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// content, _ = fs.ReadFile("usr/nested")
-	// fmt.Println(string(content))
-
-	// fs.CreateFile("/usr/share/testing", []byte("testing root files"))
-
-	// content, _ = fs.ReadFile("/usr/share/testing")
-	// fmt.Println(string(content))
-
-	// fs.CreateDir("/usr/share/fog")
-	// fs.ChangeDir("/usr/share/fog")
-
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// // shouldn't work as the directories preceding it don't exist
-	// fs.CreateDir("/usr/share/fog/this/shouldnt-work")
-	// fs.ChangeDir("/usr/share/fog/this/shouldnt-work")
-
-	// fs.DeleteFile("/usr/share/testing")
-	// // shouldn't work as the file has been deleted
-	// content, _ = fs.ReadFile("/usr/share/testing")
-
-	// fs.ChangeDir("..")
-	// fmt.Println("fs: changed dir to: " + fs.PrintWorkingDirectory())
-
-	// items, _ := fs.ListDirectoryContents(fs.PrintWorkingDirectory())
-
-	// for k, v := range items {
-	// 	fmt.Printf("%v - %+v\n", k, v)
-	// }
-
-	// fs.DeleteDirectory("/usr/share/fog")
-	// // shouldn't work as the directory has been deleted
-	// items, err := fs.ListDirectoryContents("/usr/share/fog")
-	// fmt.Println(err)
-
-	// fs.CreateFile("kernel2", []byte("hey!"))
-	// fs.EditFile("kernel2", []byte("hey again!"))
-	// content, _ = fs.ReadFile("kernel2")
-	// fmt.Println(string(content))
-
-	// fmt.Println(fs.PrintWorkingDirectory())
-	// items, _ = fs.ListDirectoryContents(fs.PrintWorkingDirectory())
-
-	// for k, v := range items {
-	// 	fmt.Printf("%v - %+v\n", k, v)
-	// }
+	for k, v := range items {
+		fmt.Printf("%v - %+v\n", k, v)
+	}
 }

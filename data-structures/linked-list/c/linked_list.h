adca3ea827b9e4b4f25a42a8618513141c57141d
Simo Aleksandrov <simo3003@me.com>
2019-11-16 00:24:44 +0200

    ll

--- data-structures/linked-list/c/linked_list.h
+++ data-structures/linked-list/c/linked_list.h
@@ -1,4 +1,13 @@
 typedef struct Node {
-    int value;
+    int Value;
     struct Node *Next;
 } Node;
+
+void add(Node* n, int v) {
+    Node *next = n->Next;
+
+    Node temp;
+    temp.Value = v;
+
+    n->Next = &temp;
+} 
--- data-structures/linked-list/c/main.c
+++ data-structures/linked-list/c/main.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 #include "./linked_list.h"
 
-int main(int argc, char *argv[]) {
+int main(void) {
     Node node; 
-    node.value = 1;
+    node.Value = 1;
 
     Node node1;
-    node1.value = 2;
+    node1.Value = 2;
 
     node.Next = &node1;
 
-    printf("%i\n", node.value);
-    printf("%i\n", node.Next->value);
+    printf("%i\n", node.Value);
+    printf("%i\n", node.Next->Value);
+    printf("before adding\n");
+
+    add(&node, 5);
+
+    printf("after adding\n");
+    printf("%i\n", node.Value);
+    printf("%i\n", node.Next->Value);
+    printf("%i\n", node.Next->Next->Value);
 
     return 0;
 }

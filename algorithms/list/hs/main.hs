15a1c036e1e8cc7424f1558e885cdd76ce2c0e47
Simo Aleksandrov <simo3003@me.com>
2019-12-24 01:25:09 +0200

    empty

--- algorithms/list/hs/main.hs
+++ algorithms/list/hs/main.hs
@@ -7,13 +7,13 @@ mkEmpty :: List a
 mkEmpty = Empty 
 
 foldl' :: (a -> b -> b) -> b -> List a -> b
-foldl' _ _ Empty = error "empty list"
+foldl' _ d Empty = d
 foldl' f d (Fork x Empty) = f x d
 foldl' f d (Fork x xs) = foldl' f (f x d) xs
 -- f (f (f (f (a1 d) a2) a3) a4)
 
 foldr' :: (a -> b -> b) -> b -> List a -> b
-foldr' _ _ Empty = error "empty list"
+foldr' _ d Empty = d
 foldr' f d (Fork x Empty) = f x d 
 foldr' f d (Fork x xs) = f x (foldr' f d xs)
 -- f (a1 f (a2 f (a3 f (a4 d))))

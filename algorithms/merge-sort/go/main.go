6bbbdcf8632643d869f43257be60bc79ca8cc214
Simo Aleksandrov <simo3003@me.com>
2020-05-30 18:12:30 +0300

    MERGE SORT

--- algorithms/merge-sort/go/main.go
+++ algorithms/merge-sort/go/main.go
@@ -4,21 +4,47 @@ import "fmt"
 
 func main() {
 	arr := []int{4, 5, 12, 6, 3, 8, 11, 15, 1, 2}
-	mergeSort(arr, 0, len(arr)-1)
+	arr = mergeSort(arr)
 	fmt.Println(arr)
 }
 
-func mergeSort(arr []int, low, high int) {
-	if low < high {
-		pivot := (low + high) / 2
-		mergeSort(arr, low, pivot-1)
-		mergeSort(arr, pivot+1, high)
-		merge(arr, low, pivot, high)
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
 	}
+
+	pivot := len(arr) / 2
+	a := mergeSort(arr[:pivot])
+	b := mergeSort(arr[pivot:])
+	return merge(a, b)
 }
 
-func merge(arr []int, low, pivot, high int) {
-	for _, num := range arr {
+func merge(a []int, b []int) []int {
+	length := len(a) + len(b)
+	sorted := make([]int, 0, length-2)
 
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			sorted = append(sorted, a[i])
+			i++
+		} else {
+			sorted = append(sorted, b[j])
+			j++
+		}
 	}
+
+	// drain the last elements
+	for i < len(a) {
+		sorted = append(sorted, a[i])
+		i++
+	}
+
+	// drain the last elements
+	for j < len(b) {
+		sorted = append(sorted, b[j])
+		j++
+	}
+
+	return sorted
 }

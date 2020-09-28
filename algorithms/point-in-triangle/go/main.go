96c8d4646f7a20ebee99bae5e97146805913dd4f
Simo Aleksandrov <simo3003@me.com>
2020-09-29 00:28:16 +0300

    add a false example

--- algorithms/point-in-triangle/go/main.go
+++ algorithms/point-in-triangle/go/main.go
@@ -17,6 +17,14 @@ func main() {
 			image.Pt(2, 2),
 		),
 	)
+	fmt.Println(
+		isPointInTriangleDet(
+			image.Pt(1, 1),
+			image.Pt(3, 1),
+			image.Pt(1, 3),
+			image.Pt(2, 4),
+		),
+	)
 }
 
 func isPointInTriangleDet(p1, p2, p3 image.Point, p image.Point) bool {

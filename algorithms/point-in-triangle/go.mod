5c736ad27b5a205503ceda23d8213d258e433860
Simo Aleksandrov <simo3003@me.com>
2020-09-28 22:45:15 +0300

    finish det solution

--- algorithms/point-in-triangle/go.mod
+++ algorithms/point-in-triangle/go.mod
@@ -1,3 +1,5 @@
 module point-in-triangle
 
 go 1.15
+
+require github.com/fr3fou/matrigo v0.0.0-20200928192006-5e745a325d46
--- algorithms/point-in-triangle/main.go
+++ algorithms/point-in-triangle/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"image"
+	"math"
+
+	"github.com/fr3fou/matrigo"
 )
 
 func main() {
 	fmt.Println(
-		isPointInTriangle(
+		isPointInTriangleDet(
 			image.Pt(1, 1),
 			image.Pt(3, 1),
 			image.Pt(1, 3),
@@ -16,6 +19,32 @@ func main() {
 	)
 }
 
-func isPointInTriangle(p1, p2, p3 image.Point, point image.Point) bool {
-	return false
+func isPointInTriangleDet(p1, p2, p3 image.Point, p image.Point) bool {
+	mainArea := matrigo.New(3, 3, [][]float64{
+		{float64(p1.X), float64(p1.Y), 1},
+		{float64(p2.X), float64(p2.Y), 1},
+		{float64(p3.X), float64(p3.Y), 1},
+	}).Det() / 2
+
+	sum := 0.0
+
+	sum += math.Abs(matrigo.New(3, 3, [][]float64{
+		{float64(p.X), float64(p.Y), 1},
+		{float64(p2.X), float64(p2.Y), 1},
+		{float64(p3.X), float64(p3.Y), 1},
+	}).Det() / 2)
+
+	sum += math.Abs(matrigo.New(3, 3, [][]float64{
+		{float64(p1.X), float64(p1.Y), 1},
+		{float64(p.X), float64(p.Y), 1},
+		{float64(p3.X), float64(p3.Y), 1},
+	}).Det() / 2)
+
+	sum += math.Abs(matrigo.New(3, 3, [][]float64{
+		{float64(p1.X), float64(p1.Y), 1},
+		{float64(p2.X), float64(p2.Y), 1},
+		{float64(p.X), float64(p.Y), 1},
+	}).Det() / 2)
+
+	return mainArea == sum
 }

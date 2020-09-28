4a7442bfb7d4a3e57b701708874079c810ad2946
Simo Aleksandrov <simo3003@me.com>
2020-09-28 21:35:11 +0300

    begin working on isPointInTriangle

--- algorithms/point-in-triangle/main.go
+++ algorithms/point-in-triangle/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(
+		isPointInTriangle(
+			image.Pt(1, 1),
+			image.Pt(3, 1),
+			image.Pt(1, 3),
+			image.Pt(2, 2),
+		),
+	)
+}
+
+func isPointInTriangle(p1, p2, p3 image.Point, point image.Point) bool {
+	return false
 }
